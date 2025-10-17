5a08998e9dc54d85eac47cbc3fdcfd5a64fceb1a
Caruso013 <pedromkk13@gmail.com>
2025-10-17 17:43:51 -0300

    chore(router): use HashRouter to prevent dev 404 on deep links

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
@@ -26,27 +26,27 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
-          
+
           {/* Médico Routes */}
           <Route path="/medico/dashboard" element={<DoctorDashboard />} />
           <Route path="/medico/perfil" element={<DoctorProfile />} />
-          
+
           {/* Admin Routes */}
           <Route path="/admin/medicos" element={<AdminMedicos />} />
           <Route path="/admin/consultas" element={<AdminConsultas />} />
           <Route path="/admin/criar-atendente" element={<CriarAtendente />} />
-          
+
           {/* Atendente Routes */}
           <Route path="/atendente" element={<AttendantDashboard />} />
-          
+
           {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
